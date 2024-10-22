801519803fc490cc8828d3475cd730d164f01868
emmano2997 <emmanuel.cavalcanti2003@gmail.com>
2024-10-22 11:39:06 -0300

    Updates

--- receivingSocketUDP.py
+++ receivingSocketUDP.py
@@ -1,12 +1,31 @@
-import socket
+from socket import *
 
-UDP_IP = "177.37.173.165" #teste local "15.228.191.109"
-UDP_PORT = 5010
+inputFiles = input("Entre com o endereço IP e o número da porta: ")
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+x = inputFiles.split()
 
-while True:
-    data, addr = sock.recvfrom(1024) 
-    print("received message: %s" % data)
+if(len(x) < 2):
+    print("Usando configuração padrão:")
+    severName =  "177.37.173.165"
+    severPort = 12345
+else:
+    severName = x[0]
+    severPort = int(x[1])
+
+# Cria o socket usando o protocolo IPV4 e UDP
+clienteSocket = socket(AF_INET, SOCK_DGRAM)
+
+while(True):
+    message = input("Entre com a mensagem a ser enviada (ou 'quit' para sair): ")
+    
+    if(message.decode("utf-8")== "quit"):
+        print("Saindo...")
+        break
+
+    auxMessage = message.encode('utf-8')
+    clienteSocket.sendto(auxMessage,(severName,severPort))
+    
+    modifiedMessage,severAddress = clienteSocket.recvfrom(2048)
+    print("Endereço do sevidor: ", severAddress)
+    print(modifiedMessage.decode("utf-8"))
+clienteSocket.close()
--- sendingSocketUDP.py
+++ sendingSocketUDP.py
@@ -1,15 +1,30 @@
-import socket
-import datetime
-agora = datetime.datetime.now()
+from socket import *
 
-UDP_IP = "127.0.0.1" #teste local "15.228.191.109"
-UDP_PORT = 50000
-MESSAGE = b"Hello, World!"
+inputFiles = input("Entre com o endereço IP e o número da porta (ou pressione Enter para usar o padrão): ")
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
+x = inputFiles.split()
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+if(len(x) < 2):
+    print("Usando configuração padrão:")
+    serverName =  "177.37.173.165"
+    serverPort = 12345
+else:
+    serverName = x[0]
+    serverPort = int(x[1])
+    
+                     # protocolo IPV4 usando o UDP
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((serverName, serverPort))
+
+print("O servidor está pronto para receber")
+
+while (True):
+    menssage,clienteAddress = serverSocket.recvfrom(2048) #recebe a mensagem e o endereço do cliente 
+    if(menssage.decode("utf-8")== "quit"):
+        print("Saindo...")
+        break
+    print("Endreço do cliente",clienteAddress)
+    print("Mensagem vinda do clinete: ",menssage.decode("utf-8"))
+    
+    menssageAux = "sever: " + menssage.decode("utf-8")
+    serverSocket.sendto(bytes(menssageAux,'ascii'),clienteAddress)
