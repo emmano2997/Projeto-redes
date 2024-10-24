a7dbaff3dc632f3d824398b4ca331a8f3e7f4d29
emmano2997 <emmanuel.cavalcanti2003@gmail.com>
2024-10-24 14:53:03 -0300

    Update Scapy

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from scapy.all import *
+from scapy.config import conf
+from scapy.layers.inet import IP, UDP
+from scapy.sendrecv import send
 
-# Exemplo de captura de pacotes
-def captura_pacotes():
-    pacotes = sniff(count=10)
-    pacotes.show()
+iface = conf.iface
+ip = IP(src=iface.ip, dst='127.0.0.1')  # IP de destino maquina local
 
-if __name__ == "__main__":
-    captura_pacotes()
+# Definindo as portas de origem e destino
+udp = UDP(sport=50000, dport=12345)  # Porta de origem (12345) e destino (8080)
+
+data = input(int(""))
+
+send(ip/udp/data)
