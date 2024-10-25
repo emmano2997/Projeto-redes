2661990e35dbe16cbda003387a1c52ebaa2e2355
Livia FM <annaliviafmachado@gmail.com>
2024-10-25 19:49:45 -0300

    Atualizando cabeçalho UDP

--- clienteScapy.py
+++ clienteScapy.py
@@ -7,10 +7,10 @@ payload = b'\x02\x5C\xE1'  # payload de 3 bytes
 # 0x025CE1 (req da quant de respostas enviadas pelo server, com id 23777)
 
 # cabeçalho IP
-ip_header = IP(src = '187.64.55.75', dst = '15.228.191.109', len=8 + len(payload), chksum=0)
+ip_header = IP(src = '187.64.55.75', dst = '15.228.191.109')
 
 # cabeçalho UDP
-udp_header = UDP(sport = org_port, dport = dest_port)
+udp_header = UDP(sport = org_port, dport = dest_port, len=8 + len(payload), chksum=0)
 # porta de origem, porta de destino, comprimento do segmento (8 + 3 de payload), checksum que inicia 0
 
 # pacote
